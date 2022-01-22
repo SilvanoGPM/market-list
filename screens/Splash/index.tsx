40c18e2ddd85f0947be29274d3c644890401de16
Silvano Marques <silvanosilvino@hotmail.com>
2022-01-22 05:55:53 -0300

    fix: splash screen bug fixed

--- screens/Splash/index.tsx
+++ screens/Splash/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
 import { StackActions } from '@react-navigation/native';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import LottieView from 'lottie-react-native';
@@ -6,17 +6,38 @@ import LottieView from 'lottie-react-native';
 type SplashProps = NativeStackScreenProps<RootStackParamList, 'Splash'>;
 
 export function Splash({ navigation }: SplashProps): JSX.Element {
-  function goToHome(): void {
+  const [timerID, setTimerID] = useState<NodeJS.Timeout>();
+  const ref = useRef<LottieView>(null);
+
+  const goToHome = useCallback(() => {
+    if (timerID) {
+      clearTimeout(timerID);
+    }
+
     navigation.dispatch(StackActions.replace('Home'));
-  }
+  }, [navigation, timerID]);
+
+  useEffect(() => {
+    const newTimerID = setTimeout(() => {
+      navigation.dispatch(StackActions.replace('Home'));
+    }, 4000);
+
+    setTimerID(newTimerID);
+  }, [navigation]);
+
+  useEffect(() => {
+    if (ref.current) {
+      ref.current.play();
+    }
+  }, []);
 
   return (
     <LottieView
       style={{
         flex: 1,
       }}
+      ref={ref}
       source={require('../../assets/splash.json')}
-      autoPlay
       speed={0.8}
       loop={false}
       onAnimationFinish={goToHome}
